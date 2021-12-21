4501f36b46495313e46df4b44a89a38b1ff26214
Andy Rowland <programmerandy@gmail.com>
2021-12-21 18:52:07 -0500

    saving test queries to benchmark oracle against postgres

---
+++ TestQueries.sql
@@ -0,0 +1,20 @@
+--Postgres
+ \timing
+ select prod.name, prod.productnumber, proch.enddate, proch.standardcost, proch.modifieddate
+ from production.product prod
+   inner join production.productcosthistory proch on proch.productid  = prod.productid
+   inner join production.transactionhistory thist on thist.productid  = prod.productid
+   inner join production.workorder word on word.productid = prod.productid 
+   inner join production.workorderrouting wordr on wordr.workorderid  = word.workorderid
+ order by prod.name;
+
+ --Oracle
+ set lines 200
+set pagesize 200
+col customer_name format 80
+select cust_last_name || ',' || ' ' || cust_first_name as customer_name, cnt.country_name, prod.prod_name
+from sh.sales sls
+  inner join sh.customers cust on cust.cust_id = sls.cust_id
+  inner join sh.countries cnt  on cust.country_id = cnt.country_id
+  inner join sh.products prod on prod.prod_id = sls.prod_id
+order by customer_name, prod_name;
